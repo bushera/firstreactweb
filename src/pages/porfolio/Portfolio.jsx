56f3ffed6af5ecd05dd02dec6931c9be9ea74e69
BushEra <busheracompanies@gmail.com>
2021-11-24 04:59:07 +0100

    latest commit

---
+++ src/pages/porfolio/Portfolio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Portfolio.css';
+
+
+class Portfolio extends  React.Component{
+    render(){
+        return(
+            <h1>this is my portfolio</h1>
+        )
+    }
+}
+
+
+export default Portfolio; 
